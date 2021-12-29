fa4825bbaa8772b38fbce057c0bb37eee4eed94a
Ariel Grosh <ariel241202@gmail.com>
2021-12-29 18:49:23 +0200

    added extended mutation class

--- mutations.py
+++ mutations.py
@@ -18,4 +18,37 @@ class SimpleMutation(BaseMutation):
     @staticmethod
     def mutate(input_data: bytes) -> bytes:
         index = random.randint(0, len(input_data))
-        return input_data[:index] + os.urandom(4) + input_data[index + 1:]
+        return input_data[:index] + os.urandom(1) + input_data[index + 1:]
+
+
+class Mutation3Choices(BaseMutation):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def delete_byte(input_data: bytes, index: int) -> bytes:
+        if index == len(input_data):
+            index = random.randint(0, len(input_data)-1)
+        return input_data[:index]+input_data[index+1:]
+
+    @staticmethod
+    def insert_byte(input_data: bytes, index: int) -> bytes:
+        random_byte = os.urandom(1)
+        return input_data[:index] + random_byte + input_data[index:]
+
+    @staticmethod
+    def flip_byte(input_data: bytes, index: int) -> bytes:
+        random_byte = os.urandom(1)
+        return input_data[:index] + random_byte + input_data[index+1:]
+
+    @staticmethod
+    def mutate(input_data: bytes) -> bytes:
+        choices = [
+            Mutation3Choices.delete_byte,
+            Mutation3Choices.insert_byte,
+            Mutation3Choices.flip_byte,
+        ]
+
+        selected_mutator = random.choice(choices)
+        index = random.randint(0, len(bytes))
+        return selected_mutator(input_data, index)
