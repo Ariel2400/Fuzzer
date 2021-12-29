04805c084dc2f522857f75f8a11f08854b1f57fd
Ariel Grosh <ariel241202@gmail.com>
2021-12-29 17:41:37 +0200

    added Simple mutation class that implements a very basic mutation algorithm

--- mutations.py
+++ mutations.py
@@ -1,7 +1,21 @@
+import random
+import os
+
+
 class BaseMutation:
     def __init__(self):
         pass
 
-    @classmethod
-    def mutate(cls):
+    @staticmethod
+    def mutate(input_data: bytes) -> bytes:
         pass
+
+
+class SimpleMutation(BaseMutation):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def mutate(input_data: bytes) -> bytes:
+        index = random.randint(0, len(input_data))
+        return input_data[:index] + os.urandom(4) + input_data[index + 1:]
