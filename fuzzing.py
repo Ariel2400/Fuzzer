a88ec0482aff0af50330c226368629f5fbc56570
Tomer Royf <tomer12901@gmail.com>
2021-12-29 21:17:47 +0200

    fuzz splited signals

--- fuzzing.py
+++ fuzzing.py
@@ -1,11 +1,16 @@
 import mutations
-
+import glob 
+import subprocess
+import os
+import hashlib
+import random
+import time
+import threading
 
 class SimpleFuzzer:
-    def __init__(self, sample_dir_path, crashes_dir_path, num_of_threads = 1):
+    def __init__(self, sample_dir_path, crashes_dir_path):
         self.sample_dir_path = sample_dir_path
         self.crashes_dir_path = crashes_dir_path
-        self.num_of_threads = num_of_threads
         self.samples = self.load_samples()
         self.amount_of_fuzzings = 0
 
@@ -14,29 +19,98 @@ class SimpleFuzzer:
     there may be two diffrent files with same content, address that.
     '''
     def load_samples(self):
-
-        return {}
+        corpus_filenames = glob.glob(self.sample_dir_path + "/*")
+        corpus = set()
+        sample_content_dict = {}
+        for filename in corpus_filenames:
+            previous_corpus_size = len(corpus)
+            file_content = open(filename, 'rb').read()
+            corpus.add(file_content)
+            if len(corpus) != previous_corpus_size:
+                sample_content_dict[filename] = file_content 
+        return sample_content_dict
 
     ''' 
-    create a new file with sample_content and run it on the target_command_line 
+    create a new file with content and run it on the target_command_line 
     target command line args is in the format: [[target], [-args]].
     '''
-    def fuzz(self, output_file, sample_content, target_command_line_args, sample_name):
-        assert isinstance(output_file, str)
-        assert isinstance(sample_content, bytearray)
+    def fuzz(self, file_save_fuzz_content, content, target_command_line_args, sample_name):
+        assert isinstance(file_save_fuzz_content, str)
+        assert isinstance(content, bytearray)
         assert isinstance(target_command_line_args, list)
         assert isinstance(sample_name, str)
         # run the target on the sample content with args,
         # if crashed, document the crash in a file and put it in self.crashes_dir
-        pass
+        
+        with open(file_save_fuzz_content, "wb") as fd:
+            fd.write(content)
+        
+        sp = subprocess.Popen(target_command_line_args + [file_save_fuzz_content],
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL,)
+        
+        ret = sp.wait()
+        if ret != 0:
+            print(f"Exited with {ret}")
+            hash = hashlib.sha256(content).hexdigest()
+            if ret == -11:
+                #SIGSEGV - Invalid memory reference
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGSEGV:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -6:
+                #SIGABRT - Abort signal from abort()
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGABRT:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -7:
+                #SIGBUS - Bus error (bad memory access)
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGBUS:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -8:
+                #SIGFPE - Floating-point exception
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGFPE:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -4:
+                #SIGILL - Illegal Instruction
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGILL:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -31:
+                #SIGSYS - Bad system call
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGSYS:{sample_name.split('/')[-1]}"), "wb")
+            if ret == -24:
+                #SIGXCPU - CPU time limit exceeded
+                open(os.path.join(self.crashes_dir_path, f"crash_{hash:64}_SIGXCPU:{sample_name.split('/')[-1]}"), "wb")
+           
+
+
     '''
-    open num_of_threads threads and in each thread run fuzz with the samples
+    iterate over samples and fuzz them
     '''
-    def batch_fuzz(self, target_command_line_args):
-        self.fuzz(output_file='', sample_content='', target_command_line_args=[], sample_name='')
+    def fuzz_worker(self, target_command_line_args):
+        assert isinstance(target_command_line_args, list)
+        start_time = time.time()
+        threading.Thread(target=self.print_statiscs, args=[start_time]).start()
+        while True:
+            sample_path = random.choice(list(self.samples.keys()))
+            sample_content = bytearray(self.samples[sample_path])
+
+            for _ in range(10):
+                sample_content[random.randint(0, len(sample_content) - 1)] = random.randint(0, 255)
+            
+            self.fuzz("thd_0", sample_content, target_command_line_args, sample_path)
+            self.amount_of_fuzzings += 1
+
+
+
     '''
     print to stdout the statistics about the fuzzing so far
     '''
-    def print_statiscs(self):
-        print('')
+    def print_statiscs(self, start_time):
+        while True:
+            time.sleep(2)
+            elapsed = time.time() - start_time
+            fscp = float(self.amount_of_fuzzings) / elapsed
+            print(f"[{elapsed}] cases {self.amount_of_fuzzings} | fcps {fscp}")
 
+
+if __name__ == '__main__':
+    #fuzz = SimpleFuzzer("corpus", "crashes")
+    fuzz = SimpleFuzzer("corpus2", "crashes2")
+    #fuzz_sample = list(fuzz.samples.keys())[0]
+    #print("fuzzing on: ", fuzz_sample)
+    #uzz.fuzz("thd_0", fuzz.samples[fuzz_sample], ["objdump", "-d"], fuzz_sample)
+    fuzz.fuzz_worker(["objdump", "-x"])
