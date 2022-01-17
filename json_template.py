2d9f538ff869c73cbc7d54b893f2d35bfe1a4376
Tomer Royf <tomer12901@gmail.com>
2022-01-17 19:25:32 +0200

    added grammar template class

--- json_template.py
+++ json_template.py
@@ -1,7 +1,13 @@
+
 from typing import TypeVar, Generic
+from enum import Enum
+
 
 T = TypeVar('T')
 
+class Endian(Enum):
+    BIG = 0
+    LITTLE = 1
 
 class GrammarTemplate:
     def __init__(self, arrayOfGrammarValues:list):
@@ -11,9 +17,16 @@ class GrammarTemplate:
         pass
 
 class GrammarType(Generic[T]):
+    def __init__(self, random:bool, size:int, val:T, endian:Endian):
+        self.random = random
+        self.size = size
+        self.val = val
+        self.endian = endian
+
     def __init__(self, random:bool, size:int, val:T):
         self.random = random
         self.size = size
         self.val = val
+        self.endian = None
 
     
