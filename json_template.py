3ba73eadb352383bc55bbbff41c36c996cc9f38c
Ariel Grosh <ariel241202@gmail.com>
2022-01-31 22:12:12 +0200

    fixed json and updated project to 3.9

--- json_template.py
+++ json_template.py
@@ -1,38 +1,45 @@
-
-
-
 from inspect import signature
 from typing import TypeVar, Generic
 from enum import Enum
 from uu import decode
-import random 
+import random as rand
 import re
 import pyjson5
 import random
 
 T = TypeVar('T')
 
+
 class Endian(Enum):
-    BIG = 0
-    LITTLE = 1
+    BIG = 1
+    LITTLE = 2
+
+
+class Format(Enum):
+    NULL_TERMINATED = 1
+
+
 class GrammarType(Generic[T]):
-    def __init__(self, random:bool, size:int = None, val:T = None, endian:Endian = None, isBits:bool = False, format_string:str = None):
+    def __init__(self, random: bool, size: int = None, val: T = None, endian: Endian = None, isBits: bool = False,
+                 format_string: Format = None):
         self.random = random
         self.size = size
         self.val = val
         self.endian = endian
         self.isBits = isBits
         self.format_string = format_string
+    # if format string: format string && val
+    # if random: isBits? random byte (size) times :
+
 
 class GrammarTemplate:
-    def __init__(self, arrayOfGrammarValues:list):
+    def __init__(self, arrayOfGrammarValues: list[GrammarType]):
         self.arrayOfGrammarValues = arrayOfGrammarValues
-
 
     @staticmethod
     def computeGrammarFunction(json_string, function_string):
         function_string = function_string.replace(' ', '')
-        
+
         if function_string.isnumeric():
             return int(function_string)
 
@@ -50,27 +57,27 @@ class GrammarTemplate:
 
         elif signature == 'size':
             block = body[1:-1]
-            compute_val =  json_string[block]["size"]
+            compute_val = json_string[block]["size"]
 
         return compute_val
 
     @staticmethod
-    def createGrammarTemplateOfBlock(size = None, data = None, endian = None, isBits = False):
+    def createGrammarTemplateOfBlock(size=None, data=None, endian=None, isBits=False):
         arrayGrammarValues = []
 
-        if data == None:
-            arrayGrammarValues += [GrammarType(random = True, size = size, isBits = isBits)]
+        if not data:
+            arrayGrammarValues += [GrammarType(random=True, size=size, isBits=isBits)]
         elif type(data) == int:
             endian_form = Endian.LITTLE
-            if endian != None:
+            if endian:
                 endian_form = endian
-            arrayGrammarValues += [GrammarType(random = False, size = size, val = data, endian = endian_form, isBits = isBits)]
+            arrayGrammarValues += [GrammarType(random=False, size=size, val=data, endian=endian_form, isBits=isBits)]
         elif type(data) == str:
             if size > len(data):
-                arrayGrammarValues += [GrammarType(random = False, size = len(data), val = data, isBits = isBits)]
-                arrayGrammarValues += [GrammarType(random = True, size = size - len(data, isBits = isBits))]
+                arrayGrammarValues += [GrammarType(random=False, size=len(data), val=data, isBits=isBits)]
+                arrayGrammarValues += [GrammarType(random=True, size=size - len(data), isBits=isBits)]
             else:
-                arrayGrammarValues += [GrammarType(random = False, size = size, val = data, isBits = isBits)]
+                arrayGrammarValues += [GrammarType(random=False, size=size, val=data, isBits=isBits)]
 
         return arrayGrammarValues
 
@@ -78,7 +85,7 @@ class GrammarTemplate:
     def createGrammarTemplateFromJsonString(json_string, start_block):
         arrayGrammarValues = []
 
-        #byte_block
+        # byte_block
         if json_string[start_block]["type"] == "byte_block":
             data = None
             endian = None
@@ -88,9 +95,10 @@ class GrammarTemplate:
                 data = GrammarTemplate.computeGrammarFunction(json_string, json_string[start_block]["data-func"])
             if "endian" in json_string[start_block]:
                 endian = json_string[start_block]["endian"]
-            arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size = json_string[start_block]["size"], data = data, endian = endian)
+            arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size=json_string[start_block]["size"],
+                                                                               data=data, endian=endian)
 
-        #range_byte_block
+        # range_byte_block
         elif json_string[start_block]["type"] == "range_byte_block":
             data = None
             endian = None
@@ -102,78 +110,88 @@ class GrammarTemplate:
                 data = GrammarTemplate.computeGrammarFunction(json_string, json_string[start_block]["data-func"])
             if "endian" in json_string[start_block]:
                 endian = json_string[start_block]["endian"]
-            arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size = size, data = data, endian = endian)
-        
-        #str_block
-        elif json_string[start_block]["type"] == "str_block":
-            arrayGrammarValues += GrammarTemplate(random = False, format_string = json_string[start_block]["format"], val = json_string[start_block]["content"])
+            arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size=size, data=data, endian=endian)
 
-        #multi_bit_block
+        # str_block
+        elif json_string[start_block]["type"] == "str_block":
+            arrayGrammarValues += GrammarType(random=False, format_string=json_string[start_block]["format"],
+                                              val=json_string[start_block]["content"])
+
+        # multi_bit_block
         elif json_string[start_block]["type"] == "multi_bit_block":
             bit_blocks = json_string[start_block]["bit_blocks"]
             for bit_block in bit_blocks:
                 data = None
                 if data in json_string[bit_block]:
                     data = json_string[bit_block]["data"]
-                arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size = json_string[bit_block]["size"], data = data, isBits = True)
-                
-        #multi_byte_block
+                arrayGrammarValues += GrammarTemplate.createGrammarTemplateOfBlock(size=json_string[bit_block]["size"],
+                                                                                   data=data, isBits=True)
+
+        # multi_byte_block
         elif json_string[start_block]["type"] == "multi_byte_block":
             byte_blocks = json_string[start_block]["byte_blocks"]
             for byte_block in byte_blocks:
                 arrayGrammarValues += GrammarTemplate.createGrammarTemplateFromJsonString(json_string, byte_block)
 
-        #duplicate_block
+        # duplicate_block
         elif json_string[start_block]["type"] == "duplicate_block":
             size = json_string[start_block]["size"]
             block = json_string[start_block]["block"]
             blockTemplate = GrammarTemplate.createGrammarTemplateFromJsonString(json_string, block)
             arrayGrammarValues += blockTemplate * size
-        
-        #range_duplicate_block
+
+        # range_duplicate_block
         elif json_string[start_block]["type"] == "range_duplicate_block":
-            block = json_string[start_block]["block"] 
+            block = json_string[start_block]["block"]
             range = json_string[start_block]["range"]
             size = random.randint(range[0], range[1])
             blockTemplate = GrammarTemplate.createGrammarTemplateFromJsonString(json_string, block)
             arrayGrammarValues += blockTemplate * size
 
-        #choice_block
+        # choice_block
         elif json_string[start_block]["type"] == "choice_block":
             blocks = json_string[start_block]["blocks"]
-            arrayGrammarValues += GrammarTemplate.createGrammarTemplateFromJsonString(json_string, random.choice(blocks))
-        
-        return arrayGrammarValues
+            arrayGrammarValues += GrammarTemplate.createGrammarTemplateFromJsonString(json_string,
+                                                                                      random.choice(blocks))
 
+        return arrayGrammarValues
 
     @staticmethod
     def createGrammarTemplateFromFile(jsonFileName):
         objFile = open(jsonFileName, 'r')
         decoded_json = pyjson5.decode_io(objFile, None, False)
-        if("main_template" not in decoded_json):
+        if "main_template" not in decoded_json:
             print("dont have main_template block!")
             return
-        
+
         return GrammarTemplate(GrammarTemplate.createGrammarTemplateFromJsonString(decoded_json, "main_template"))
 
+    def create_file(self, path):
+        with open(path, 'wb') as f:
+            buffer = b''
+            for i, element in enumerate(self.arrayOfGrammarValues):
+                if element.random and not element.isBits:
+                    f.write(random.randbytes(element.size))
+                elif element.random and element.isBits and len(buffer)%8 != 0:
+                    buffer.join([random.choice([b'1', b'0']) for _ in range(element.size)])
+                else:
+                    f.write(str(element.val)[:element.size].encode())
 
-    def create_file(self, filename, path):
-        pass
 
-
-    
 
 if __name__ == '__main__':
-    template = GrammarTemplate.createGrammarTemplateFromFile("bmp.json5")
+    t = GrammarTemplate.createGrammarTemplateFromFile("bmp.json5")
+    # temp = GrammarTemplate(arr)
+    t.create_file('check2.bin')
     i = 0
-    for grammarInstance in template.arrayOfGrammarValues:
-        print("random = ", grammarInstance.random)
-        print("size = ", grammarInstance.size)
-        print("val = ", grammarInstance.val)
-        print("endian = ", grammarInstance.endian)
-        print("isBits = ", grammarInstance.isBits)
-        print("format_string = ", grammarInstance.format_string)
-        print("\n\n")
-        if i == 8:
-            break
-        i += 1
+    # for grammarInstance in template.arrayOfGrammarValues:
+    #     print("random = ", grammarInstance.random)
+    #     print("size = ", grammarInstance.size)
+    #     print("val = ", grammarInstance.val)
+    #     print("endian = ", grammarInstance.endian)
+    #     print("isBits = ", grammarInstance.isBits)
+    #     print("format_string = ", grammarInstance.format_string)
+    #     print("\n\n")
+    #     if i == 8:
+    #         break
+    #     i += 1
