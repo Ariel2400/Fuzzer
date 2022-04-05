0f4eb5a7cef4c870dd6722dea244298984a271e3
Ariel Grosh <ariel241202@gmail.com>
2022-04-05 18:08:37 +0300

    added library for c++

--- Json Parsers/C++ defualt implementation/test.cpp
+++ Json Parsers/C++ defualt implementation/test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
 
+int main(int argc, char const *argv[])
+{
+    json json = json::parse(argv[1])
+    return 0;
+}
+
 // https://github.com/nlohmann/json#integration
 // need to integrate it
