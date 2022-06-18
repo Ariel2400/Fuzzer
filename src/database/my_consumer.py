a2db05d7041f0c38ab3d2e87099952c2c11e94f5
Ariel Grosh <ariel241202@gmail.com>
2022-06-18 11:37:33 +0300

    added waiting for messages

--- src/database/my_consumer.py
+++ src/database/my_consumer.py
@@ -7,6 +7,14 @@ class MyConsumer:
     def __init__(self) -> None:
         self._consumer = KafkaConsumer('JSON Samples', bootstrap_servers='localhost:9092', group_id=None, value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-    def get_batch(self) -> Dict:
-        return self._consumer.poll(max_records=10)
+    def get_batch(self, num_of_records) -> Dict:
+        while True:
+            b = self._consumer.poll(max_records=num_of_records)
+            if b.values():
+                return b
+            else:
+                continue
+
+
+
 
