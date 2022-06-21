97eb5b8be73c947fe0f0c514718ed242b62774b6
Tomer Royf <tomer12901@gmail.com>
2022-06-21 19:39:38 +0300

    added generation without kafka

--- src/FileGenerator/SymbolicExecutionGenerator.py
+++ src/FileGenerator/SymbolicExecutionGenerator.py
@@ -1,17 +1,28 @@
 from FileGenerator import AbstractBaseFileGenerator
 from kafka_handlers.consumer import Consumer
 from symbolic_execution.symbolic_execution_producer import SymbolicExecutionProducer
+from symbolic_execution.symbolic_execution_engine import SymbolicExecution
+from typing import Union
 
 class SymbolicExecutionGenerator(AbstractBaseFileGenerator.AbstractBaseFileGenerator):
 
-    def __init__(self, symbolic_execution_producer: SymbolicExecutionProducer):
+    def __init__(self, symbolic_execution_producer: Union[SymbolicExecutionProducer, None], symbolic_execution_engine: Union[SymbolicExecution, None]):
         self.consumer = Consumer()
         self.symbolic_execution_producer = symbolic_execution_producer
+        self.stop_generator = False
+        self.symbolic_execution_engine = symbolic_execution_engine
+        if symbolic_execution_engine is not None:
+            self.generator = self.symbolic_execution_engine.getTargetInput()
   
     def generateData(self) -> bytes:
-        batches = self.consumer.get_batch(1)
-        print("batches = ", batches)
-        data = list(batches.values())[0][0].value
+        if self.stop_generator:
+            raise Exception("tried to generate data after stopping")
+        if self.symbolic_execution_producer is not None:
+            batches = self.consumer.get_batch(1)
+            data = list(batches.values())[0][0].value
+        else:
+            data = next(self.generator)
+        
         return data
 
     def generateFile(self, path: str):
@@ -21,4 +32,6 @@ class SymbolicExecutionGenerator(AbstractBaseFileGenerator.AbstractBaseFileGener
         fileObj.close()
 
     def close(self):
-        self.symbolic_execution_producer.stopProduce()
+        self.stop_generator = True
+        if self.symbolic_execution_producer is not None:
+            self.symbolic_execution_producer.stopProduce()
