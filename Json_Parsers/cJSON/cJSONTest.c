35ca9b883116c9ce5f9185dd201a42e77622d497
Ariel2400 <ariel241202@gmail.com>
2022-06-22 19:51:13 +0300

    fix?

--- Json_Parsers/cJSON/cJSONTest.c
+++ Json_Parsers/cJSON/cJSONTest.c
@@ -6,14 +6,7 @@
 
 int main(int argc, char const *argv[])
 {
-    struct stat stat_buf;
-    int fd;
-    fd = open(argv[1], O_RDONLY);
-    fstat(fd, &stat_buf);
-    int flen=stat_buf.st_size;
-    char* json_f_buffer = (char*)malloc(flen);
-    FILE *fp = fopen(argv[1], "r");
-    fread(json_f_buffer, flen, 1, fp);
+    char *json_f_buffer = argv[1];
     cJSON *json = cJSON_Parse(json_f_buffer);
     char *string = cJSON_Print(json);
     cJSON_Delete(json);
