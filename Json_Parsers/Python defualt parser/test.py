a320be3d32fef1e5763250de8b3b8f4541efce2a
Tomer Royf <tomer12901@gmail.com>
2022-06-23 21:46:59 +0300

    removed uneccessary files

--- Json_Parsers/Python defualt parser/test.py
+++
@@ -1,8 +0,0 @@
-import json
-import sys
-
-if __name__ == '__main__':
-    json_file = sys.argv[1]
-    with open(json_file, 'r') as f:
-        json_string = json.loads(f.read())
-    print(json_string)
